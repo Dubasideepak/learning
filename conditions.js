1fb73b4f78ad780e237be0186db308b62971b1a6
Dubasi Deepak <deepak31dubasi@gmail.com>
2023-12-09 15:46:34 +0530

    conditions

--- conditions.js
+++ conditions.js
@@ -15,6 +15,7 @@ var num = 10;
 var condition1 = 100;
 var condition2 = 200;
  var condition3 = (condition1 - condition2);
+ var condition4 = 600;
  if (condition3 > condition1)
   {
     console.log("true");
@@ -22,3 +23,27 @@ var condition2 = 200;
   else{
     console.log("false");
   }
+   
+  // Logical Operators
+  //two types and their symbols -- AND(&&),OR(||)
+   //  AND &&
+   // T & T = T
+   // T & F = F
+   // F & T = F
+   // F & F = F
+            //and
+        var value = condition2 > condition1 & condition4 ;
+          if (value){
+            console.log("this is correct");
+          }
+          else {
+            console.log("this is wrong");
+          }
+          //or
+          var value = condition2 > condition1 || condition4 ;
+          if (value){
+            console.log("this is correct");
+          }
+          else {
+            console.log("this is wrong");
+          }
