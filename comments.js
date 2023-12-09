469ef4dd0318c03e16f71669c1bdf475f39b1455
Dubasi Deepak <deepak31dubasi@gmail.com>
2023-12-09 13:34:43 +0530

    comments

---
+++ comments.js
@@ -0,0 +1,22 @@
+  comments are two types:
+  
+1.Single line comment: 
+
+  A Single line comment is denoted by using '//'.
+  Anything after '//' on line will be considered 
+  a comment and won't executed as code by the JavaScript engine.
+
+  Ex: //This is a single line comment
+      let variable = 123; // This comment is at the end of a line of a code.
+  
+ 2.Multi line comment:
+   
+   we can create multi line comment using '/*000000000*/'.
+   This allows us to comment multiple lines.
+
+ Ex:/*This is a multi line comment 
+ let variable = 10; 
+ console.log(variable);
+ */
+
+
