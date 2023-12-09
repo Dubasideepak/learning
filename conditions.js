5def07e631f7a0ee806f29c376f23aa716d9b0c9
Dubasi Deepak <deepak31dubasi@gmail.com>
2023-12-09 15:13:49 +0530

    if.condition

---
+++ conditions.js
@@ -0,0 +1,24 @@
+//if condition
+  //ex1
+var num = 10;
+
+  if (num > 0)
+  {
+    console.log("true");
+  }
+  else
+  {
+    console.log("false");
+  }
+
+  //ex2
+var condition1 = 100;
+var condition2 = 200;
+ var condition3 = (condition1 - condition2);
+ if (condition3 > condition1)
+  {
+    console.log("true");
+  }
+  else{
+    console.log("false");
+  }
