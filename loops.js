fedc395dc8f98a0cc113e7ed1ae2650eebaf322b
Dubasi Deepak <deepak31dubasi@gmail.com>
2023-12-10 19:01:00 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,12 @@
+/*
+Q) Write a program to print squaress of odd numbers till 10 odds?
+Output:
+1,3,5,9
+1,9,25,81
+*/
+for(var i=1; i<=10; i = i+1 ){
+    if(i % 2 ){
+   var square =i*i;
+    console.log(square);
+    }
+}
