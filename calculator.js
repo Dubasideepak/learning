b43b1c33759dd523e6ad7e8841019716204581f1
Dubasi Deepak <deepak31dubasi@gmail.com>
2023-12-18 23:03:29 +0530

    calculator.js

---
+++ calculator.js
@@ -0,0 +1,33 @@
+/*
+write a program to create a calculator?
+*/
+var num1 = 5;
+  var num2 = 10;
+function add(a, b) {
+    return a + b;
+  }
+  console.log("Addition:", add(num1, num2));
+//============
+
+  function multi(a, b) {
+    return a * b;
+  }
+  console.log("multiplication:", multi(num1, num2));
+  //======
+
+  function div(a, b) {
+    return a / b;
+  }
+  console.log("division:", div(num1, num2));
+  //=======
+
+  function sub(a, b) {
+    return a - b;
+  }
+  console.log("subtraction:", sub(num1, num2));
+  //========
+
+  function mod(a, b) {
+    return a % b;
+  }
+  console.log("modulo:", mod(num1, num2));
